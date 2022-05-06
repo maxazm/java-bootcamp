fe5f8a9c3477edeb370a01db5cb4c7d02887017d
maxazm <zomk0222@gmail.com>
2022-05-06 15:09:18 +0900

    initial commit for section one

---
+++ section-one/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("RRRR       A");
+        System.out.println("R   R    A  A");
+        System.out.println("R   R   A    A");
+        System.out.println("RRRR    AAAAAA");
+        System.out.println("R R     A    A");
+        System.out.println("R  R    A    A");
+        System.out.println("R   R   A    A");
+    }
+}
